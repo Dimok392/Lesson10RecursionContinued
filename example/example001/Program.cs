d110dfb0ffe906d17076dc17878b4be8af7c6e0d
Пётр <dimok392@mail.ru>
2022-06-12 09:31:35 +0300

    example001

---
+++ example/example001/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2, b = 9;
+int N = 8;
+int Fib (int f, int s, int num)
+{
+    if (num == 0 ) return f;
+    if (num==1)return s;
+    return Fib(f,s,num-1)+Fib(f,s,num-2);
+
+}
+for (int i = 0; i < N; i++)
+{
+int m = Fib (a,b,i);
+Console.Write(m + " ");
+}
